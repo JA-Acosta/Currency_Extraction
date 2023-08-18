ec31e38b88609faa284d512193a9e5b024a0304a
JAAR <joe.aaron.acosta@outlook.com>
2023-08-18 11:28:31 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+>>> JAAR
+>>> 08/18/2023
+>>> Currency Code Extraction
+>>> Version 0
+'''
+
+'''
+>>> Takes a csv file containing information for currencies and extracts the currency 3 letter code and the country of origin then creates a new csv file with that information.
+'''
+
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
